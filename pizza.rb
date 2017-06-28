688729931b1a6343c692c2675e1fd8daa27e7297
Ldeems <luke.deems@yahoo.com>
2017-06-28 15:43:36 -0400

    sauce if working

--- pizza.rb
+++ pizza.rb
@@ -20,35 +20,30 @@ def meat
 	def suasage?
 		if 1 == [1,2].sample
 			puts "suasage"
-		
 		end	
 	end	
 	meat << suasage?
 	def bacon?
 		if 1 == [1,2].sample
 			puts "bacon"
-		
 		end		
 	end	
 	meat << bacon?
 	def pepperoni?
 		if 1 == [1,2].sample
 			puts "pepperoni"
-		
 		end
 	end			
 	meat << pepperoni?
 	def ham?
 		if 1 == [1,2].sample
 			puts "ham"
-		
 		end
 	end	
 	
 	def chicken?
 		if 1 == [1,2].sample
-			puts " chicken"
-		
+			puts "chicken"
 		end
 	end
 	meat << chicken?			
@@ -60,7 +55,6 @@ def veggies
 	def mushrooms?
 		if 1 == [1,2].sample
 			puts "mushrooms"
-		
 		end 
 	end
 	veggies << mushrooms?
@@ -68,7 +62,6 @@ def veggies
 	def olives?
 		if 1 == [1,2].sample
 			puts "olives"
-		
 		end
 	end
 	veggies << olives?
@@ -76,7 +69,6 @@ def veggies
 	def peppers?
 		if 1 == [1,2].sample
 			puts "peppers"
-		
 		end
 	end
 	veggies << peppers?			
@@ -84,23 +76,24 @@ def veggies
 	def onions?
 		if 1 == [1,2].sample
 			puts "onions"
-		
 		end
 	end
 	veggies << onions?		
 end
 
-def special
-	extra meat 
-	ertra cheese
-	extra sauce 
-	no cheese
-end
 
-def sauce
-	normal
-	ranch
-	bbq
+def sauce()
+	x = 1 + rand(3)
+	if
+		x == 1
+		x = "normal"
+	elsif 
+		x == 2
+		x = "ranch"
+	else
+		x == 3
+		x = "bbq"
+	end	
 end
 
 def pizza
@@ -108,6 +101,7 @@ def pizza
 end	
 
 
-#	p crust
-#	p meat
-	p veggies
+#	puts crust
+#	puts meat
+#	puts veggies
+	puts sauce
