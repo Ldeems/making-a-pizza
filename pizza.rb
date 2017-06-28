b19db2b0f001f3e14a47210ace55dfb37ead4bde
Ldeems <luke.deems@yahoo.com>
2017-06-28 14:17:11 -0400

    got crust working

---
+++ pizza.rb
@@ -0,0 +1,51 @@
+
+
+def crust
+	crust = [one,two,three].sample
+end
+def one	
+	one  =  "thin"
+end	
+def two
+	two =  "thick"
+end
+def three
+	three =  "stuffed"
+end	
+
+
+def meat 
+	suasage
+	bacon
+	peperoni
+	ham
+	chicken
+
+end
+
+def veggies 
+	mushrooms
+	olives
+	banna peppers
+	onions
+
+end
+
+def special
+	extra meat 
+	ertra cheese
+	extra sauce 
+	no cheese
+end
+
+def sauce
+	normal
+	ranch
+	bbq
+end
+
+def pizza
+	print "yay pizza"
+end	
+
+p crust
